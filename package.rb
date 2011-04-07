ded83c083647af0d35e6bcc4e338451a2207058e
Julia <imaginejulia@gmail.com>
2011-04-07 11:10:14 +0300

    test4

--- package.rb
+++ package.rb
@@ -1,2 +1,26 @@
 #package.rb
+class Package
 
+    attr_accessor :weight
+    attr_accessor :state 
+    #attr_accessor :id
+
+    def initialize(weight = nil, state = nil)
+        @weight = weight
+	@state = state
+	#@address = address
+	#@id = nil
+    end
+#    def set_name(name)
+ #       @name = name
+  #  end
+    
+   # def set_surename(surename)
+    #    @surename = surename
+    #end
+  
+   # def set_address(address)
+    #    @address = address
+    #end
+    
+end
