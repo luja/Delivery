7f81ca050b0a0dda985a51579ae194bcb2dfe731
Julia <imaginejulia@gmail.com>
2011-04-07 03:45:50 +0300

    Client file

---
+++ client.rb
@@ -0,0 +1,5 @@
+#client.rb
+
+class Client
+end
+
---
+++ client_spec.rb
@@ -0,0 +1,10 @@
+#client_spec.rb
+
+require './client' 
+
+describe Client do
+    it "initializes" do
+        person = Person.new
+    end
+end
+
