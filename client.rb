50902038509213ad7df2359d5c33e2882ddbf123
Julia <imaginejulia@gmail.com>
2011-04-07 04:31:06 +0300

    test2

--- client.rb
+++ client.rb
@@ -1,10 +1,18 @@
 #client.rb
 
 class Client
-   # attr_accessor :name
-    
-    def initialize()
-       # @name = name
+
+    attr_accessor :name
+    #attr_accessor :surname 
+    #attr_accessor :address
+    #attr_accessor :id
+
+    def initialize()#, surname = nil, address = nil)
+        @name = nil
+	#@surname = surname
+	#@address = address
+	#@id = nil
     end
+    
 end
 
--- client_spec.rb
+++ client_spec.rb
@@ -3,12 +3,17 @@
 require './client' 
 
 describe Client do
-    it "initializes" do
-        client = Client.new
+    before :each do
+      @client = Client.new
     end
-    it "should set name" do
-        client = Client.new
-        client.name = "Jonas"
+    it "should initially have nil" do
+        @client.name.should == nil
     end
+  #  it "should set surname" do
+   #     @client.surname = "Jonas"
+    #end
+   # it "should set address" do
+    #    @client.surname = "Laimes al.34, Vilnius"
+    #end
 end
 
