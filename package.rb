ae0562480c4622e7cf2efa5d22688fe3838b3865
Julia <imaginejulia@gmail.com>
2011-04-28 12:15:28 +0300

    test

--- package.rb
+++ package.rb
@@ -7,49 +7,51 @@ PACKAGE_TYPE = ["Letter", "Small package", "Medium package", "Big package"]
 
 class Package
 
-    @weight
-    @state
-    @price
-    @type
-    attr_reader :weight
-    attr_reader :state 
-    attr_reader :price
-    attr_reader :type
+  @weight
+  @state
+  @price
+  @type
+  attr_reader :weight
+  attr_reader :state 
+  attr_reader :price
+  attr_reader :type
     
-    def initialize(weight = nil, state = nil)
-      @weight = weight
-      @state = PACKAGE_STATE[0]
-      self.set_type
-      self.count_price
-    end
-    def set_weight(weight)
-      @weight = weight
-      self.set_type
-    end
-    
-    def set_state(state)
-      @state = state
-    end
-    
-    def count_price()
-      @price = PRICE + PRICE_COF * @weight
-    end
-    def next_state()
-      @state = PACKAGE_STATE[PACKAGE_STATE.index(@state) + 1]
-      if @state == nil 
-        @state = PACKAGE_STATE[2]
-      end
-    end
+  def initialize(weight = nil, state = nil)
+    @weight = weight
+    @state = PACKAGE_STATE[0]
+    self.set_type
+    self.count_price
+  end
 
-    def set_type()
-      if @weight <= 300
-        @type = PACKAGE_TYPE[0]
-        elsif @weight <= 800
-          @type = PACKAGE_TYPE[1]
-          elsif @weight <= 1500
-            @type = PACKAGE_TYPE[2]
-          else @type = PACKAGE_TYPE[3] 
-      end
+  def set_weight(weight)
+    @weight = weight
+    self.set_type
+  end
+  
+  def set_state(state)
+    @state = state
+  end
+    
+  def count_price()
+    @price = PRICE + PRICE_COF * @weight
+  end
+
+  def next_state()
+    @state = PACKAGE_STATE[PACKAGE_STATE.index(@state) + 1]
+    if @state == nil 
+      @state = PACKAGE_STATE[2]
     end
+  end
+
+  def set_type()
+    if @weight <= 300
+      @type = PACKAGE_TYPE[0]
+      elsif @weight <= 800
+        @type = PACKAGE_TYPE[1]
+        elsif @weight <= 1500
+          @type = PACKAGE_TYPE[2]
+        else @type = PACKAGE_TYPE[3] 
+    end
+  end
 
 end
--- package_spec.rb
+++ package_spec.rb
@@ -5,72 +5,76 @@
 require './package'
 
 describe Package do
-    before :each do
-      @package = Package.new(100)
-    end
-    it "should initialize with weigh and price" do
-      @package.weight.should == 100
-      @package.price.should > 0
-    end
+  before :each do
+    @package = Package.new(100)
+  end
+  it "should initialize with weigh and price" do
+    @package.weight.should == 100
+    @package.price.should > 0
+  end
+    it "should initialize with sender and receiver eqqual to nil" do
+    @package.sender.should == nil
+    @package.receiver.should == nil
+  end
     #++
-    it "should set to first state when initialized" do
-        @package.state.should == "Registered"
-    end
-      it "should initialize with proper type not nil" do
-      @package.type.should_not  == nil
-    end
-    it "should initialize with state registered" do
+  it "should set to first state when initialized" do
       @package.state.should == "Registered"
-    end
-    it "should change state registered into given to curier" do
-      @package.next_state()
-      @package.state.should == "Given to curier"
-    end
-    it "should change state Given to curier into registered" do
-      @package.next_state()
-      @package.next_state()
-      @package.state.should == "Delivered"
-    end
-    it "should not change state delivered" do
+  end
+  it "should initialize with proper type not nil" do
+    @package.type.should_not  == nil
+  end
+  it "should initialize with state registered" do
+    @package.state.should == "Registered"
+  end
+  it "should change state registered into given to curier" do
+    @package.next_state()
+    @package.state.should == "Given to curier"
+  end
+  it "should change state Given to curier into registered" do
+    @package.next_state()
+    @package.next_state()
+    @package.state.should == "Delivered"
+  end
+  it "should not change state delivered" do
     @package.next_state()
     @package.next_state()
     @package.next_state()
     @package.state.should == "Delivered"
-    end
-    it "should set weight" do
-      @package.set_weight(350)
-      @package.weight.should == 350
-    end 
+  end
+  it "should set weight" do
+    @package.set_weight(350)
+    @package.weight.should == 350
+  end 
 
-    it "should set state" do
-      @package.set_state("Delivered")
-      @package.state.should == "Delivered"
-    end 
-    #++
-    it "should count price" do
-      @package.set_weight(1000)
-      @package.count_price 
-      @package.price.should == 300.5
-    end
-    it "should set price even if package weight 0 g" do
-      @package.set_weight(0)
-      @package.count_price
-      @package.price.should == PRICE
-    end
-    it "should set a letter type if weight is under 300 g" do
-      @package.set_weight(230)
-      @package.type.should == "Letter"
-    end
-    it "should set a medium package type if weight is 800 - 1500 g" do
-      @package.set_weight(1000)
-      @package.type.should == "Medium package"
-    end
-    it "should set a small package type if weight is above 300 and under 800 g" do
-      @package.set_weight(750)
-      @package.type.should == "Small package"
-    end
-     it "should set a big package type if weight is above 1500 g" do
-      @package.set_weight(2000)
-      @package.type.should == "Big package"
-    end
+  it "should set state" do
+    @package.set_state("Delivered")
+    @package.state.should == "Delivered"
+  end 
+  #++
+  it "should count price" do
+    @package.set_weight(1000)
+    @package.count_price 
+    @package.price.should == 300.5
+  end
+  it "should set price even if package weight 0 g" do
+    @package.set_weight(0)
+    @package.count_price
+    @package.price.should == PRICE
+  end
+  it "should set a letter type if weight is under 300 g" do
+    @package.set_weight(230)
+    @package.type.should == "Letter"
+  end
+  it "should set a medium package type if weight is 800 - 1500 g" do
+    @package.set_weight(1000)
+    @package.type.should == "Medium package"
+  end
+  it "should set a small package type if weight is above 300 and under 800 g" do
+    @package.set_weight(750)
+    @package.type.should == "Small package"
+  end
+  it "should set a big package type if weight is above 1500 g" do
+    @package.set_weight(2000)
+    @package.type.should == "Big package"
+  end
 end 
